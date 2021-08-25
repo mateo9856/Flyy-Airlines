51baaf69dc8a8a09e6a10a9a288e08d35e48d202
Mateusz M <mateo9656@gmail.com>
2021-08-25 23:08:47 +0200

    25 08 change

--- FlyyAirlines/ClientApp/src/components/Chat.jsx
+++ FlyyAirlines/ClientApp/src/components/Chat.jsx
@@ -2,7 +2,6 @@
 import { MdExitToApp } from 'react-icons/md';
 import "../css/Chat.css";
 import FetchDatas from "../FetchDatas";
-import "../css/ChatRoom.css";
 import "@microsoft/signalr";
 import { HubConnectionBuilder } from '@microsoft/signalr';
 import { AppContext } from "../AppContext";
@@ -22,10 +21,15 @@ const Chat = (props) => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        connection.invoke("SendMessage", UserConnection, Values.content).catch(err => console.log("Error not send!"))
+        connection.invoke("SendMessage", UserConnection, Values.content).then(() => 
+            ChatHistory.push({
+                user: selectedUser,
+                content: Values.content
+            })//not load repair!
+        ).catch(err => console.log("Error not send!"))
         
     }
-
+    console.log(ChatHistory);
     const [ActiveUsers, SetActiveUsers] = useState([]);
 
     const [selectedUser, setSelectedUser] = useState("");
@@ -55,10 +59,20 @@ const Chat = (props) => {
         
         setConnection(newConnection);
     }, [])
-
-    console.log(connection);
+    const ChatExit = () => {
+        connection.stop();
+        props.exit(false);
+    }
 
     const handleChange = (e) => {
+        if (!e.target.value) {
+            return;
+        }
+        if (e.target.name === "user") {
+            const GetUserName = ActiveUsers.filter(el => el.connectionId === e.target.value);
+            setSelectedUser(GetUserName[0].userName);
+        }
+
         setValues({
             ...Values,
             [e.target.name]: e.target.value
@@ -68,24 +82,24 @@ const Chat = (props) => {
     return (
         <div className="chatBox">
             <div>
-                <button className="chatExit" onClick={() => props.exit(false)}><MdExitToApp style={{ fontSize: "26px" }} /></button>
+                <button className="chatExit" onClick={ChatExit}><MdExitToApp style={{ fontSize: "26px" }} /></button>
             </div>
             <form className="chatFormFlex" onSubmit={handleSubmit}>
-                <div className="d-flex h-100 flex-row align-items-stretch">
+                <div className="d-flex chatHeight flex-row align-items-stretch">
                 <div className="w-25 p-2 listUsers">
-                    {ActiveUsers &&
-                            <select multiple className="form-control form-control-sm" value={Values.user} onChange={handleChange}>
-                            {ActiveUsers.map(data => <option value={data.connectionId}>{data.userName}</option>)}
+                        {ActiveUsers &&
+                            <select style={{ height: "400px", overflow: "hidden" }} multiple className="form-control form-control-sm" value={Values.user} name="user" onChange={handleChange}>
+                                {ActiveUsers.map(data => <option value={data.connectionId}>{data.userName}</option>)}
                         </select>}
                 </div>
                     <div className="w-75 p-2 chatContent">
                         <div className="userInfo">
-                            <p className="h5 text-center">{selectedUser}</p>
+                            <p className="h5 text-center">{selectedUser ? `Rozmowa z: ${selectedUser}` : "Wybierz osobę"}</p>
                         </div>
-                        <div className="messageBox">
+                        <div className="messageBox h-50 p-2">
                             {ChatHistory && ChatHistory.map(data => <></>)}{/*Add messagesStyle*/}
                         </div>
-                    <textarea className="form-control" rows="2" type="text" name="content" value={Values.content} onChange={handleChange} />
+                    <textarea className="form-control chatTextStyle" rows="2" type="text" name="content" value={Values.content} onChange={handleChange} />
                     </div>
                 </div>
                 <div className= "form-group bottomMargin p-2">
