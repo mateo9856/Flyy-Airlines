f89c146ec0b24af0aeb89334c6d61693e2440c0e
Mateusz M <mateo9656@gmail.com>
2021-08-30 19:53:29 +0200

    CSV reader and work to calculate time to fly

--- FlyyAirlines.Repository/FlightsAirplanes/AirplanesFlightsData.cs
+++ FlyyAirlines.Repository/FlightsAirplanes/AirplanesFlightsData.cs
@@ -1,6 +1,9 @@
 ﻿using FlyyAirlines.Models;
+using LumenWorks.Framework.IO.Csv;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,7 +18,32 @@ namespace FlyyAirlines.Repository.FlightsAirplanes
             _dbContext = dbContext;
         }
 
-        public async Task<bool> CheckReservesFromFlights(Reservation reservation, Flight flight)
+        public void CalculateFlightTime(string fromCity, string fromCountry, string toCountry, string toCtiy)
+        {
+            var csvTable = new DataTable();
+
+            using (var csvReader = new CsvReader(new StreamReader(File.OpenRead(Path.Combine(Directory.GetCurrentDirectory(), "FlightsCitiesData.csv"))), true))
+            {
+                csvTable.Load(csvReader);
+            }
+
+            List<DataRow> searchDatas = new List<DataRow>();
+
+            for(int i = 0; i < csvTable.Rows.Count;i++)
+            {
+                if(csvTable.Rows[i][1].ToString() == fromCity && csvTable.Rows[i][4].ToString() == fromCountry || csvTable.Rows[i][1].ToString() == toCtiy && csvTable.Rows[i][4].ToString() == toCountry)
+                {
+                    searchDatas.Add(csvTable.Rows[i]);
+                }
+                
+                decimal[] fromLatLng = { decimal.Parse(searchDatas[0].ItemArray[2].ToString()), decimal.Parse(searchDatas[0].ItemArray[2].ToString()) };
+                decimal[] toLatLng = { decimal.Parse(searchDatas[1].ItemArray[2].ToString()), decimal.Parse(searchDatas[0].ItemArray[1].ToString()) };
+            }
+            //think pattern to calculate time
+
+        }
+
+            public async Task<bool> CheckReservesFromFlights(Reservation reservation, Flight flight)
         {
             var getFlight = await _dbContext.Flights.FindAsync(flight);
             var checkReservation = getFlight.Reservations.FirstOrDefault(res => res == reservation);
